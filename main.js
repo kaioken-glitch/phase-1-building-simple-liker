7440d576a7e58167d04465aee236c8c4b573a19f
Cyprian <lightningcyranus@gmail.com>
2025-06-20 07:28:26 +0200

    hid the error modal and change the heart in between empty and full

--- main.js
+++ main.js
@@ -4,7 +4,49 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorModal = document.getElementById('modal');
+errorModal.className = 'hidden';
 
+const hearts = document.querySelectorAll('.like-glyph');
+
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    /* 
+    mimicServerCall is a function that simulates a server call and we invoke it
+    here
+    */ 
+    mimicServerCall()
+      .then(() => {
+        // If the server call is successful, toggle the heart state
+        // between empty and full heart
+        // and add or remove the 'activated-heart' class accordingly
+        heart.classList.remove('hidden');
+        
+        // Toggle the heart state
+        // If the heart is empty, change it to full and vice versa
+        // Also, add or remove the 'activated-heart' class
+        heart.innerText = heart.innerText === FULL_HEART ? EMPTY_HEART : FULL_HEART;
+        // If the heart is empty, change it to full and vice versa
+        // Also, add or remove the 'activated-heart' class
+        // This is a more concise way to toggle the heart state
+        // and manage the class
+        //if (heart.innerText === EMPTY_HEART) {
+        //  heart.innerText = FULL_HEART;
+        //  heart.classList.add('activated-heart');
+        //} else {
+        //  heart.innerText = EMPTY_HEART;
+        //  heart.classList.remove('activated-heart');
+        //}
+      })
+      .catch(error => {
+        errorModal.className = '';
+        errorModal.innerText = error;
+        setTimeout(() => {
+          errorModal.className = 'hidden';
+        }, 3000);
+      });
+  });
+});
 
 
 //------------------------------------------------------------------------------
